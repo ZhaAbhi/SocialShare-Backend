4bf74ab423fe5ff88a831a5971546389c151820d
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-06-12 14:30:24 +0545

    Added trim property to true in user schema

--- src/models/user/user.mongo.js
+++ src/models/user/user.mongo.js
@@ -5,14 +5,17 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
+      trim: true,
     },
     password: {
       type: String,
       required: true,
+      trim: true,
     },
   },
   { timestamps: true }
